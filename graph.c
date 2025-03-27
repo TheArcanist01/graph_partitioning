08f03681401329a37dc1f679c6b0b833591f905a
TheArcanist <99442890+TheArcanist01@users.noreply.github.com>
2025-03-27 18:35:15 +0100

    modified node definition + added load from file function

--- graph.c
+++ graph.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
 
 // struktura węzła
 
 typedef struct node {
 
     int Number;
-    struct node *Connections;
+    struct node **Connections;
 
 } node_t;
 
@@ -25,14 +27,33 @@ typedef struct graph {
 
 // funkcja czytająca graf z pliku
 
-graph_t *load_from_file (FILE *Stream) {
-    return NULL;
+graph_t *new_graph (int Height, int Width, node_t *Nodes, int *NodeIndexes) {
+
+    graph_t *NewGraph = malloc(sizeof(graph_t));
+
+    NewGraph->Height = Height;
+    NewGraph->Width = Width;
+    NewGraph->Nodes = Nodes;
+    NewGraph->NodeIndexes = NodeIndexes;
+
+    return NewGraph;
 }
 
-// algorytm dzielący graf
+node_t new_node (int Number, node_t **Connections) {
+    node_t NewNode;
+    
+    NewNode.Number = Number;
+    NewNode.Connections = Connections;
 
-graph_t **partition_graph (graph_t *Graph, int PartitionCount, double ErrorMargin) {
-    return NULL;
+    return NewNode;
+}
+
+void free_graph (graph_t *Graph) {
+    for (int i = 0; i < (int) sizeof(Graph->NodeIndexes)/sizeof(int); i++) {
+        free(Graph->Nodes[i].Connections);
+    }
+    free(Graph->Nodes);
+    free(Graph);
 }
 
 // print graph
@@ -67,7 +88,164 @@ void print_graph (graph_t *map) {
     }
 }
 
+graph_t *load_from_file (FILE *Stream) {
+
+    if (Stream == NULL) {
+        fprintf(stderr, "[!] Error: Can't open file!\n");
+        return NULL;
+    }
+
+    char Width[64];                 // max 64B
+    char NodeIndexes[65536];        // max 64kB
+    char FirstIndexes[65536];
+    char Connections[65536];
+    char ConnectionIndexes[65536];
+
+    fgets(Width, 64, Stream);
+    fgets(NodeIndexes, 65536, Stream);
+    fgets(FirstIndexes, 65536, Stream);
+    fgets(Connections, 65536, Stream);
+    fgets(ConnectionIndexes, 65536, Stream);
+
+    int GraphWidth = atoi(Width);
+
+    int GraphNodeIndexes[65536];
+
+    char *NodeIndex = NodeIndexes;
+    int CurrentIndex = 0;
+    while (*NodeIndex) {
+        if (isdigit(*NodeIndex)) {
+            long Value = strtol(NodeIndex, &NodeIndex, 10);
+            GraphNodeIndexes[CurrentIndex] = Value;
+            CurrentIndex++;
+        } else {
+            NodeIndex++;
+        }
+    }
+    GraphNodeIndexes[CurrentIndex] = -1;
+
+    int GraphFirstIndexes[65536];
+
+    char *FirstIndex = FirstIndexes;
+    CurrentIndex = 0;
+    while (*FirstIndex) {
+        if (isdigit(*FirstIndex)) {
+            long Value = strtol(FirstIndex, &FirstIndex, 10);
+            GraphFirstIndexes[CurrentIndex] = Value;
+            CurrentIndex++;
+        } else {
+            FirstIndex++;
+        }
+    }
+    GraphFirstIndexes[CurrentIndex] = -1;
+
+    int GraphConnections[65536];
+
+    char *Connection = Connections;
+    CurrentIndex = 0;
+    while (*Connection) {
+        if (isdigit(*Connection)) {
+            long Value = strtol(Connection, &Connection, 10);
+            GraphConnections[CurrentIndex] = Value;
+            CurrentIndex++;
+        } else {
+            Connection++;
+        }
+    }
+    GraphConnections[CurrentIndex] = -1;
+
+    int GraphConnectionIndexes[65536];
+
+    char *ConnectionIndex = ConnectionIndexes;
+    CurrentIndex = 0;
+    while (*ConnectionIndex) {
+        if (isdigit(*ConnectionIndex)) {
+            long Value = strtol(ConnectionIndex, &ConnectionIndex, 10);
+            GraphConnectionIndexes[CurrentIndex] = Value;
+            CurrentIndex++;
+        } else {
+            ConnectionIndex++;
+        }
+    }
+    GraphConnectionIndexes[CurrentIndex] = -1;
+    
+    node_t *NewGraphNodes = malloc(65536 * sizeof(node_t));
+    int NewGraphNodeIndexes[65536];
+    
+    int CurrentNodeIndex = 0;
+    int CurrentNodeNumber = 0;
+    int NewGraphHeight = 0;
+    while(GraphFirstIndexes[NewGraphHeight] != -1) {
+        if (GraphFirstIndexes[NewGraphHeight + 1] == GraphFirstIndexes[NewGraphHeight]) {
+            NewGraphHeight++;
+            continue;
+        }
+        while(GraphFirstIndexes[NewGraphHeight + 1] != CurrentNodeNumber && GraphFirstIndexes[NewGraphHeight + 1] != -1) {
+            NewGraphNodes[CurrentNodeNumber] = new_node(CurrentNodeNumber, NULL);
+            NewGraphNodeIndexes[CurrentNodeNumber] = GraphNodeIndexes[CurrentNodeIndex] + (NewGraphHeight * GraphWidth);
+            CurrentNodeIndex++;
+            CurrentNodeNumber++;
+        }
+        NewGraphHeight++;
+    }
+    NewGraphHeight--;
+    
+    graph_t *NewGraph = new_graph(NewGraphHeight, GraphWidth, NewGraphNodes, NewGraphNodeIndexes);
+
+    int GraphConnectionsIterator = 1;
+    int GraphConnectionIndexesIterator = 0;
+
+    while (GraphConnectionIndexes[GraphConnectionIndexesIterator] != -1) {
+        int CurrentNodeToMakeConnectionsFor = GraphConnections[GraphConnectionIndexes[GraphConnectionIndexesIterator]];
+        for (int i = 0; i < CurrentNodeNumber; i++) {
+            if (NewGraph->Nodes[i].Number == CurrentNodeToMakeConnectionsFor) {
+                NewGraph->Nodes[i].Connections = malloc (65536 * sizeof(node_t *));
+                int NewGraphNodeConnectionsIterator = 0;
+                while (GraphConnectionsIterator != GraphConnectionIndexes[GraphConnectionIndexesIterator + 1] /* && GraphConnectionIndexes[GraphConnectionIndexesIterator + 1] != -1 */) {
+                    if (GraphConnections[GraphConnectionsIterator] == -1) {
+                        break;
+                    }
+                    NewGraph->Nodes[i].Connections[NewGraphNodeConnectionsIterator] = &NewGraph->Nodes[GraphConnections[GraphConnectionsIterator]];
+                    NewGraphNodeConnectionsIterator++;
+                    GraphConnectionsIterator++;
+                }
+                NewGraph->Nodes[i].Connections[NewGraphNodeConnectionsIterator] = NULL;
+                CurrentNodeToMakeConnectionsFor++;
+                GraphConnectionsIterator++;
+                break;
+            }
+        }
+        GraphConnectionIndexesIterator++;
+    }
+
+    /* print_graph(NewGraph);
+
+    for (int i = 0; i < 105; i++) {
+        int j = 0;
+        while(NewGraph->Nodes[i].Connections != NULL && NewGraph->Nodes[i].Connections[j] != NULL) {
+            printf("%d -> %d\n", NewGraph->Nodes[i].Number, NewGraph->Nodes[i].Connections[j]->Number);
+            j++;
+        }
+    } 
+        
+    free_graph(NewGraph); */
+
+
+    return NewGraph;
+}
+
+// algorytm dzielący graf
+
+graph_t **partition_graph (graph_t *Graph, int PartitionCount, double ErrorMargin) {
+    return NULL;
+}
+
+
+
 // funkcje pomocnicze
+
+
+
 
 // main
 
@@ -94,9 +272,9 @@ int main (int argc, char **argv) {
 
     map->Nodes[0].Connections = malloc(2 * sizeof(node_t));
     map->Nodes[1].Connections = malloc(1 * sizeof(node_t));
-    map->Nodes[0].Connections[0] = map->Nodes[1];
-    map->Nodes[0].Connections[1] = map->Nodes[2];
-    map->Nodes[1].Connections[0] = map->Nodes[2];
+    //map->Nodes[0].Connections[0] = map->Nodes[1];
+    //map->Nodes[0].Connections[1] = map->Nodes[2];
+    //map->Nodes[1].Connections[0] = map->Nodes[2];
 
     print_graph(map);
 
@@ -108,6 +286,10 @@ int main (int argc, char **argv) {
     }
 
     free(map);
+
+    FILE *Stream = fopen("graf.cssrg", "r+");
+    load_from_file(Stream);
     
+    fclose(Stream);
     return 0;
 }
